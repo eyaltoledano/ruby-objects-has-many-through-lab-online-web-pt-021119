0c4d758b42cb2aeea846425be01bb4df7685a924
Eyal Toledano <eyaltoledano@me.com>
2019-03-23 21:04:46 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,7 @@ class Appointment
   attr_writer :patient, :doctor
   attr_accessor :date
 
-  def initialize(patient, date, doctor)
+  def initialize(patient, doctor, date)
     @patient = patient
     @date = date
     @doctor = doctor
