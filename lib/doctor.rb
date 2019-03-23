d7078b749fc9c73d3fd49f33c455b98e504eb471
Eyal Toledano <eyaltoledano@me.com>
2019-03-23 21:16:11 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -26,7 +26,7 @@ class Doctor
   end
 
   def patients
-    Appointment.all.select do |appointment|
+    Appointment.all.collect do |appointment|
       binding.pry
       appointment.patient if appointment.doctor == self)
     end
